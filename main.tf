dfb08081729fd8eb99030ab4783ba2faef6c9094
rageshganeshkumar <ragesh.ganeshkumar@hashicorp.com>
2025-06-02 09:48:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,12 @@ terraform {
 }
 
 resource "random_string" "example" {
+  count   = 100
   length  = var.length
   special = false
   upper   = false
 }
 
-output "random_string" {
-  value = random_string.example.result
+output "random_strings" {
+  value = [for s in random_string.example : s.result]
 }
