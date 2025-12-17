1945c0d08f70975d82d4cd0e3e71e1e7fd95b912
rageshganeshkumar <ragesh.ganeshkumar@hashicorp.com>
2025-12-17 23:31:47 +0530

    update

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "length" {
   description = "Length of the random string"
   type        = number
 }
+
+variable "pass" {
+  description = "A sensitive variable"
+  type        = string
+  sensitive   = true
+}
